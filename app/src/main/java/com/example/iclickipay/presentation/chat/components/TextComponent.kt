057ab98e6f7fa6f6a99f88b4a057d8645680aff6
didimoffat1999 <egm2149@columbia.edu>
2024-12-30 14:38:09 -0500

    More work on ChatsScreen

--- app/src/main/java/com/example/iclickipay/presentation/chat/components/TextComponent.kt
+++ app/src/main/java/com/example/iclickipay/presentation/chat/components/TextComponent.kt
@@ -2,18 +2,18 @@ package com.example.iclickipay.presentation.chat.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import com.example.iclickipay.R
+import androidx.compose.ui.unit.TextUnit
 
 @Composable
-fun TextComponent(value: String) {
+fun TextComponent(value: String, fontSize: TextUnit, color: Color) {
     Text(
         text = value,
         style = TextStyle(
-            fontSize = 30.sp,
+            fontSize = fontSize,
+            color = color,
             fontWeight = FontWeight.SemiBold
         )
     )
--- app/src/main/java/com/example/iclickipay/presentation/chat/components/UserDetails.kt
+++ app/src/main/java/com/example/iclickipay/presentation/chat/components/UserDetails.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.iclickipay.R
 import com.example.iclickipay.presentation.chat.domain.ChatListDataObject
 import com.example.iclickipay.presentation.chat.domain.Message
@@ -32,7 +34,14 @@ fun UserDetails(chatData: ChatListDataObject){
 @Composable
 fun MessageHeader(chatData: ChatListDataObject) {
     Row{
-        TextComponent(value = chatData.userName)
+        TextComponent(value = chatData.userName,
+            fontSize = 18.sp,
+            color = Color.Black
+        )
+        TextComponent(value = chatData.message.timeStamp,
+            fontSize = 16.sp,
+            color = Color.Gray
+        )
     }
 }
 
